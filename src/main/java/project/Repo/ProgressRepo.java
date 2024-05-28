508ca3e839ab69f2beafeb52d46d947fbd118cbb
LeThiHongNgoc <ngocb2005766@student.ctu.edu.vn>
2024-05-28 16:23:21 +0700

    tien do du an

--- src/main/java/project/Repo/ProgressRepo.java
+++ src/main/java/project/Repo/ProgressRepo.java
@@ -1,2 +1,11 @@
-package project.Repo;public class ProgressRepo {
+package project.Repo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import project.Entity.Progress;
+
+@Repository
+public interface ProgressRepo extends MongoRepository<Progress,String> {
 }
+
